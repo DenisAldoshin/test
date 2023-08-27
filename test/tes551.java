4ca468bad1ea7eaca96f86eaca6d1abde2145736
denisaldosin <denisaldoshin32@gmail.com>
2023-08-27 03:17:13 +0300

    test

--- test/tes551.java
+++ test/tes551.java
@@ -1,38 +1,51 @@
 package test;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class tes551 {
     public static void main(String[] args) {
-
         Random random = new Random();
         Integer[] arrayNumbers = new Integer[1_000_0000];
         Character[] arrayChar = new Character[1_000_0000];
         for (int i = 0; i < arrayNumbers.length; i++) {
-//            arrayChar[i] = (char) random.nextInt(0, 1);
-//            arrayNumbers[i] = random.nextInt(0, 1);
-            arrayChar[i] = (char) random.nextInt(0, 65536);
+
+            arrayChar[i] = (char) random.nextInt(0, 100_000);
             arrayNumbers[i] = random.nextInt(0, 100_000);
         }
-        int sum2 = 0;
-        long starTime2 = System.nanoTime();
-        for (int i = 0; i < arrayNumbers.length; i++) {//Cравнение без hashCode
-            if (Integer.valueOf(arrayChar[i]).equals(arrayNumbers[i])) {
-                sum2++;
-            }
-        }
-
-        System.out.println("Количество совпадений  = " + sum2 + "\nВремя работы c сравнением hashCode: " + ((System.nanoTime() - starTime2) / 1_000));
-        int sum1 = 0;
-        long starTime1 = System.nanoTime();
-        for (int i = 0; i < arrayNumbers.length; i++) { // Cравнение через hashCode
-            if (arrayChar[i].hashCode() == arrayNumbers[i].hashCode()) {
-                if (Integer.valueOf(arrayChar[i]).equals(arrayNumbers[i])) {
-                    sum1++;
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService executorService1 = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                int sum1 = 0;
+                long starTime1 = System.nanoTime();
+                for (int i = 0; i < arrayNumbers.length; i++) {
+                    if (arrayChar[i].hashCode() == arrayNumbers[i].hashCode() & Integer.valueOf(arrayChar[i]).equals(arrayNumbers[i])) {
+                        sum1++;
+                    }
                 }
+                System.out.println("Количество совпадений = " + sum1 + "\n == и hashcode: " + ((System.nanoTime() - starTime1) / 1_000));
             }
-        }
-        System.out.println("Количество совпадений = " + sum1 + "\nВремя работы без сравнения hashCode: " + ((System.nanoTime() - starTime1) / 1_000));
+        });
+        executorService.shutdown();
+        executorService1.execute(new Runnable() {
+            @Override
+            public void run() {
+                int sum2 = 0;
+                long starTime2 = System.nanoTime();
+                for (int i = 0; i < arrayNumbers.length; i++) {
+                    if (Integer.valueOf(arrayChar[i]).equals(arrayNumbers[i])) {
+                        sum2++;
+                    }
+                }
+                System.out.println("Количество совпадений  = " + sum2 + "\n Только equals: " + ((System.nanoTime() - starTime2) / 1_000));
+            }
+        });
+        executorService1.shutdown();
     }
-
 }
+
+
+
